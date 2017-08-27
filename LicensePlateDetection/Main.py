6de90a28a978c76a037ee684f8c6b9d4e790ecd2
Chad Hoang <chad.hoang@yahoo.com>
2017-08-27 02:37:41 -0500

    WIP Main.py

--- LicensePlateDetection/Main.py
+++ LicensePlateDetection/Main.py
@@ -4,8 +4,52 @@ import time
 import os
 from openalpr import Alpr
 
+#
+#   Variables
+#
+frameCaptureDelay = 10
+videoCaptureSource = 'TestData/PL_LS_Cropped.mp4'
+
+#
+#   Functions
+#
+def GetLicensePlateInfo(image):
+
+    #gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    cv2.imwrite("FrameToExtract.jpeg", image)
+
+    results = alpr.recognize_file('FrameToExtract.jpeg')
+
+    i = 0
+    for plate in results['results']:
+        i += 1
+        print("Plate #%d" % i)
+        print("   %12s %12s" % ("Plate", "Confidence"))
+        for candidate in plate['candidates']:
+            prefix = "-"
+            if candidate['matches_template']:
+                prefix = "*"
+
+            print("  %s %12s%12f" % (prefix, candidate['plate'], candidate['confidence']))
+            break
+
+    return results
+
+#
+#   Main
+#
+
+# Setup alpr
+alpr = Alpr("us", "openalpr.conf", "runtime_data")
+if not alpr.is_loaded():
+    print("Error loading OpenALPR")
+    #sys.exit(1)
+alpr.set_top_n(20)
+alpr.set_default_region("tx")
+
 # Create a VideoCapture object
-cap = cv2.VideoCapture('TestData/PL_LS_1.mp4')
+cap = cv2.VideoCapture(videoCaptureSource)
 
 # Check if camera opened successfully
 if (cap.isOpened() == False):
@@ -33,9 +77,12 @@ while(True):
     framecount += 1
 
     # Check if this is the frame closest to 10 seconds
-    frameCaptureDelay = 2
     if framecount == (framerate * frameCaptureDelay):
         framecount = 0
+
+        # test image for license plate
+        GetLicensePlateInfo(image)
+
         cv2.imshow('image',image)
 
     # Check end of video
@@ -46,31 +93,5 @@ while(True):
 cap.release()
 cv2.destroyAllWindows()
 
-alpr = Alpr("us", "openalpr.conf", "runtime_data")
-if not alpr.is_loaded():
-    print("Error loading OpenALPR")
-    #sys.exit(1)
-
-alpr.set_top_n(20)
-alpr.set_default_region("md")
-
-results = alpr.recognize_file("TestData/us-3.jpg")
-
-i = 0
-for plate in results['results']:
-    i += 1
-    print("Plate #%d" % i)
-    print("   %12s %12s" % ("Plate", "Confidence"))
-    for candidate in plate['candidates']:
-        prefix = "-"
-        if candidate['matches_template']:
-            prefix = "*"
-
-        print("  %s %12s%12f" % (prefix, candidate['plate'], candidate['confidence']))
-
 # Call when completely done to release memory
 alpr.unload()
-
-def TestFunction():
-    print ("Hello World")
-    return
