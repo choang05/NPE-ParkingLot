53e3052f33b06037e2ec0feb63f3942d2984c04d
Chad Hoang <chad.hoang@yahoo.com>
2017-09-04 18:49:35 -0500

    Occupancy detection now works on an image with an array of masked regions.

--- ParkingLotOccupancyDetection/label_image.py
+++ ParkingLotOccupancyDetection/label_image.py
@@ -1,11 +1,17 @@
+import cv2
 import tensorflow as tf
 import sys
 
-# change this as you see fit
-image_path = sys.argv[1]
+# Read the images
+image_main = cv2.imread('PL01.jpg')
 
-# Read in the image_data
-image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+image_masks = ['PL01_Slot1_mask.jpg', 
+                'PL01_Slot2_mask.jpg',
+                'PL01_Slot3_mask.jpg']
+
+# Display masked image
+#cv2.imshow("Masked Image", image_masked)
+
 
 ## Loads label file, strips off carriage return
 label_lines = [line.rstrip() for line 
@@ -21,14 +27,26 @@ with tf.Session() as sess:
     # Feed the image_data as input to the graph and get first prediction
     softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
     
-    #predictions = sess.run(softmax_tensor, \
-    #         {'DecodeJpeg/contents:0': image_data})
-    predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+    for index, element in enumerate(image_masks):
+        mask = cv2.imread(image_masks[index], 0)
+        image_masked = cv2.bitwise_and(image_main, image_main, mask = mask)
+ 
+        cv2.imwrite('image_masked.jpg', image_masked)
 
-    # Sort to show labels of first prediction in order of confidence
-    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+        # change this as you see fit
+        image_path = 'image_masked.jpg'
+
+        # Read in the image_data
+        image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+
+        #predictions = sess.run(softmax_tensor, \
+        #         {'DecodeJpeg/contents:0': image_data})
+        predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+
+        # Sort to show labels of first prediction in order of confidence
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
     
-    for node_id in top_k:
-        human_string = label_lines[node_id]
-        score = predictions[0][node_id]
-        print('%s (score = %.5f)' % (human_string, score))
+        for node_id in top_k:
+            human_string = label_lines[node_id]
+            score = predictions[0][node_id]
+            print('%s (score = %.5f)' % (human_string, score))
